7c3fb2b72e11937b547bc96099b25e8a9b2fb113
robdunn220 <robertdunn220@gmail.com>
2019-10-08 13:00:06 -0400

    Added form validation, and input specific validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms-start';
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
 }
